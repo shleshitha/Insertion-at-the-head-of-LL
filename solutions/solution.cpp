d38b5cccc9456542f9a3a682308dbc0ec8f8530d
sai krishna <saikrishnamamidi2003@gmail.com>
2025-04-21 14:44:28 +0530

    chanes in cpp

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -3,6 +3,23 @@
 #include <string>  // Required for std::string
 using namespace std;
  
+// ListNode.h
+#ifndef LISTNODE_H
+#define LISTNODE_H
+
+struct ListNode {
+    int data;
+    ListNode* next;
+
+    ListNode(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+#endif
+
+
 class Solution {
     public:
         ListNode* insertAtHead(ListNode* &head, int X) {
