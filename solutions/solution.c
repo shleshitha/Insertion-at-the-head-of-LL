6a74f0e3881f28f9964e739e6a3fa863346de033
sai krishna <saikrishnamamidi2003@gmail.com>
2025-04-21 16:21:13 +0530

    c file changed

--- solutions/solution.c
+++ solutions/solution.c
@@ -9,14 +9,5 @@ struct ListNode {
 
 
 struct ListNode* insertAtHead(struct ListNode* head, int X) {
-<<<<<<< HEAD
     //implement your code
 }
-=======
-    struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
-    newNode->val = X;
-    newNode->next = head;
-    head = newNode;
-    return head;
-}
->>>>>>> 00b203b79b53b91286a9f094bf0acf2cb6db28c6
