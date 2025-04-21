c4a453a0653ebb104129e2b0971b1d47125d68a6
saikrishnamamidi2003 <139036109+saikrishnamamidi2003@users.noreply.github.com>
2025-04-21 12:34:37 +0530

    Update solution.cpp

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -8,7 +8,7 @@ class Solution {
         ListNode* insertAtHead(ListNode* &head, int X) {
             ListNode* newNode = new ListNode(X);
             newNode->next = head;
-            head = newNode; 
+            head = newNode;  
             return head; 
         }
     };
