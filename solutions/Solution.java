f6b961654d74ec3f64affb3c8e449d61261ec4d8
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-04-22 06:41:43 +0530

    Update Solution.java

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -2,9 +2,8 @@ package solutions;
 
 public class Solution {
     public ListNode insertAtHead(ListNode head, int val) {
-        
-            //implement your code 
-        return null;
-        
+        ListNode newNode = new ListNode(val); // create a new node with the value
+        newNode.next = head; // point the new node's next to the current head
+        return newNode; // ne
     }
 }
