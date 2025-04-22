f3ce20feac363bbdc102a89bc822e0587dab7bcd
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-04-22 06:38:52 +0530

    Update solution.cpp

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -4,7 +4,6 @@
 using namespace std; 
 
 struct ListNode {
-
     int data;
     ListNode* next;
 
@@ -14,13 +13,12 @@ struct ListNode {
     }
 };
 
-
 class Solution {
-    public:
-        ListNode* insertAtHead(ListNode* &head, int X) {
-                //implement your code
-            
-             return NULL;
-        }
-    };
-    
+public:
+    ListNode* insertAtHead(ListNode* &head, int X) {
+        ListNode* newNode = new ListNode(X); // Create a new node with the given value
+        newNode->next = head; // Point new node to the current head
+        head = newNode; // Update head to new node
+        return head; // Return new head
+    }
+};
